fee3b9dde8db609379325b2a9e53ca6120ca7e13
AHMEDNABIB <nabibpallab@gmail.com>
2025-07-20 15:06:15 +0600

    feat: update repository implementation

--- lib/features/daily_news/data/repository/article_repository.dart
+++ lib/features/daily_news/data/repository/article_repository.dart
@@ -1,11 +1,42 @@
-import 'package:flutter_bloc_clean/features/daily_news/domain/entities/article.dart';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc_clean/core/constants/constants.dart';
+import 'package:flutter_bloc_clean/core/resources/data_state.dart';
+import 'package:flutter_bloc_clean/features/daily_news/data/data_resources/remote/news_api_service.dart';
+import 'package:flutter_bloc_clean/features/daily_news/data/models/article.dart';
 import 'package:flutter_bloc_clean/features/daily_news/domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(
+    this._newsApiService,
+  );
+
   @override
-  Future<List<ArticleEntity>> geNewsArticles() {
-    // Implementation of fetching news articles will go here.
-    // This is just a placeholder for now.
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: apiKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioError(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioErrorType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
+    } on DioError catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/features/daily_news/domain/repository/article_repository.dart
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_bloc_clean/core/resources/data_state.dart';
 import 'package:flutter_bloc_clean/features/daily_news/domain/entities/article.dart';
 
 abstract class ArticleRepository {
-  Future<List<ArticleEntity>> geNewsArticles();
+  Future<DataState<List<ArticleEntity>>> getNewsArticles();
 }
